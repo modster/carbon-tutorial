dd52cab7b5a79775cbdeb244c1fa674bf2629fcb
E. M. Greeff <emgreeff@datadrivendashboards.com>
2022-03-28 13:40:55 -0600

    feat(tutorial): complete step 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+// import 'core-js/modules/es.array.includes';
+// import 'core-js/modules/es.array.fill';
+// import 'core-js/modules/es.string.includes';
+// import 'core-js/modules/es.string.trim';
+// import 'core-js/modules/es.object.values';
+
+// import './polyfills/element-closest';
+// import './polyfills/element-matches';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
@@ -11,7 +20,6 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
